ed015a3f10be9c2008491476e65be3e11a01157b
joaquin hernan castillo <2087@bse.com.ar>
2021-09-26 19:28:44 -0300

    Haaciendo el Delete de Clientes

--- JLabUNO/JLabUNO/JLabUNO.Api/Controllers/CustomersController.cs
+++ JLabUNO/JLabUNO/JLabUNO.Api/Controllers/CustomersController.cs
@@ -52,6 +52,13 @@ namespace JLabUNO.Api.Controllers
                 return BadRequest();
             }
 
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == customer.User.Email);
+            if (user == null)
+            {
+                return BadRequest("Usuario no existe.");
+            }
+
+            customer.User = user;
             _context.Entry(customer).State = EntityState.Modified;
 
             try
@@ -70,7 +77,8 @@ namespace JLabUNO.Api.Controllers
                 }
             }
 
-            return NoContent();
+            //return NoContent();
+            return Ok(customer);
         }
 
         // POST: api/Customers
@@ -78,6 +86,19 @@ namespace JLabUNO.Api.Controllers
         [HttpPost]
         public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
         {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == customer.User.Email);
+            if (user == null)
+            {
+                return BadRequest("Usuario no existe.");
+            }
+
+            Customer oldCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == customer.Email);
+            if (oldCustomer != null)
+            {
+                return BadRequest("Ya hay un cliente registrado con ese email.");
+            }
+
+            customer.User = user;
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
 
--- JLabUNO/JLabUNO/JLabUNO.Shared/Pages/CustomersPage.xaml.cs
+++ JLabUNO/JLabUNO/JLabUNO.Shared/Pages/CustomersPage.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using JLabUNO.Components;
+using JLabUNO.Dialogs;
 using JLabUNO.Helpers;
 using JLabUNO.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 
@@ -50,6 +55,104 @@ namespace JLabUNO.Pages
             CustomersListView.ItemsSource = null;
             CustomersListView.Items.Clear();
             CustomersListView.ItemsSource = Customers;
+        }
+
+        private async void AddCustomerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer = new Customer();
+            CustomerDialog dialog = new CustomerDialog(customer);
+            await dialog.ShowAsync();
+            if (!customer.WasSaved)
+            {
+                return;
+            }
+
+            customer.User = MainPage.GetInstance().User;
+
+            Loader loader = new Loader("Por favor espere...");
+            loader.Show();
+            Response response = await ApiService.PostAsync("Customers", customer);
+            loader.Close();
+
+            if(!response.IsSuccess)
+            {
+                MessageDialog messagedialog = new MessageDialog(response.Message, "Error");
+                await messagedialog.ShowAsync();
+                return;
+            }
+
+            Customer newCustomer = (Customer)response.Result;
+            Customers.Add(newCustomer);
+            RefreshList();
+
+        }
+        private async void DeleteImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ContentDialogResult result = await ConfirmDeleteAsync();
+            if (result != ContentDialogResult.Primary)
+            {
+                return;
+            }
+
+            Loader loader = new Loader("Por favor espere...");
+            loader.Show();
+            Customer customer = Customers[CustomersListView.SelectedIndex];
+            Response response = await ApiService.DeleteAsync("Customers", customer.Id);
+            loader.Close();
+
+            if (!response.IsSuccess)
+            {
+                MessageDialog messageDialog = new MessageDialog(response.Message, "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            List<Customer> customers = Customers.Where(c => c.Id != customer.Id).ToList();
+            Customers = new ObservableCollection<Customer>(customers);
+            RefreshList();
+        }
+
+        private async Task<ContentDialogResult> ConfirmDeleteAsync()
+        {
+            ContentDialog confirmDialog = new ContentDialog
+            {
+                Title = "Confirmación",
+                Content = "¿Estás seguro de querer borrar el registro?",
+                PrimaryButtonText = "Sí",
+                CloseButtonText = "No"
+            };
+
+            return await confirmDialog.ShowAsync();
+        }
+        private async void EditImage_Tapped(object sender, RoutedEventArgs e)
+        {
+            Customer customer = Customers[CustomersListView.SelectedIndex];
+            customer.IsEdit = true;
+            CustomerDialog dialog = new CustomerDialog(customer);
+            await dialog.ShowAsync();
+            if (!customer.WasSaved)
+            {
+                return;
+            }
+
+            customer.User = MainPage.GetInstance().User;
+
+            Loader loader = new Loader("Por favor espere...");
+            loader.Show();
+            Response response = await ApiService.PutAsync("Customers", customer, customer.Id);
+            loader.Close();
+
+            if (!response.IsSuccess)
+            {
+                MessageDialog messageDialog = new MessageDialog(response.Message, "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            Customer newCustomer = (Customer)response.Result;
+            Customer oldCustomer = Customers.FirstOrDefault(c => c.Id == newCustomer.Id);
+            oldCustomer = newCustomer;
+            RefreshList();
         }
     }
 }
